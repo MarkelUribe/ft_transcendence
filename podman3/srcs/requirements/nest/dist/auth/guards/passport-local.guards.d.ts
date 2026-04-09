d0ee72129cb417474fbcb18956fdd9b07d62252f
Nezzuz <konataisnotreal@gmail.com>
2026-04-09 20:22:32 +0200

    Secrets working but orm doesn't

--- podman3/srcs/requirements/nest/dist/auth/guards/passport-local.guards.d.ts
+++
@@ -1,4 +0,0 @@
-declare const PassportLocalGuard_base: import("@nestjs/passport").Type<import("@nestjs/passport").IAuthGuard>;
-export declare class PassportLocalGuard extends PassportLocalGuard_base {
-}
-export {};
--- podman3/srcs/requirements/nest/dist/users/avatar-upload.interceptor.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const avatarUploadInterceptor: import("@nestjs/common").Type<import("@nestjs/common").NestInterceptor<any, any>>;
