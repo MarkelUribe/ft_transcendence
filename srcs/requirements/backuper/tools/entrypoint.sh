ce93b6bad591ab2131c9ef4df1c67f8bdf5274e0
Nezzuz <konataisnotreal@gmail.com>
2026-05-16 18:23:58 +0200

    Status web

--- srcs/requirements/backuper/tools/entrypoint.sh
+++ srcs/requirements/backuper/tools/entrypoint.sh
@@ -5,6 +5,7 @@ apt-get update
 apt-get install -y mariadb-client
 apt-get install -y rsync
 apt-get install -y cron
+apt-get install -y apache2
 chmod 444 /run/secrets/db_root_password || true
 touch /var/log/cron.log
 echo "Creating new backup"
@@ -20,5 +21,11 @@ echo '
 echo "Inserting job in root's crontab"
 chmod 755 /tmp/cronjob.sh
 echo "0 * * * * /tmp/cronjob.sh" | crontab -
+
+sed -i 's/Listen 80/Listen 8080/g' /etc/apache2/ports.conf
+sed -i 's/<VirtualHost \*:80>/<VirtualHost \*:8080>/g' /etc/apache2/sites-available/0000-default.conf
+echo "Starting Apache Web Server on port 8080..."
+source /etc/apache2/envvars
+exec apache2 -DFOREGROUND
 
 cron && tail -f /var/log/cron.log
